bfaf1b04f0c0b208dac204bbb66f583174c386ec
Anastasia <79032456178@yandex.ru>
2022-07-22 14:49:12 +0300

    homework2sem5

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях");
+            int[] array;
+            array = new int[5];
+            int i = 0;
+            int sum = 0;
+            Random random = new Random();
+            while (i < array.Length)
+            {
+                array[i] = random.Next(100, 999);
+                Console.WriteLine(array[i]);
+                i++;
+
+            }
+
+            for (i = 0; i < array.Length; i++)
+            {
+                if (i % 2 != 0) 
+                sum = sum + array[i];
+            }
+            Console.WriteLine(" сумма нечётных =" + sum);
+            Console.ReadKey();
+        }
+    }
+}
